6b25be27a3b185916049c4d899e67b12d23bbce8
drongad <85913237+drongad@users.noreply.github.com>
2021-08-18 16:16:26 -0400

    Add files via upload

---
+++ js/player.js
@@ -0,0 +1,60 @@
+class Player {
+    constructor() {
+        this.index = null;
+        this.distance = 0;
+        this.name = null;
+        this.score =0;
+        this.rank = null
+
+        
+    }
+    
+    getPlayerAtEnd(){
+        database.ref('playerAtEnd').on("value",(data)=>{
+            this.rank = data.val();
+        })
+    }
+    static updatePlayersAtEnd(rank){
+        database.ref('/').update({
+            playersAtEnd: rank
+        })
+    }
+
+    update(state) {
+        database.ref('/').update({
+            gameState: state
+        });
+    }
+    
+
+    getCount() {
+        var playerCountRef = database.ref('playerCount');
+        playerCountRef.on("value", (data) => {
+            playerCount = data.val();
+        })
+    }
+
+    updateCount(count) {
+        database.ref('/').update({
+            playerCount: count
+        });
+    }
+
+    update() {
+        var playerIndex = "players/player" + this.index;
+        database.ref(playerIndex).set({
+            name: this.name,
+            distance: this.distance,
+            score:this.score
+        });
+    }
+
+    static getPlayerInfo() {
+        var playerInfoRef = database.ref('players');
+        playerInfoRef.on("value", (data) => {
+            allPlayers = data.val();
+        })
+    }
+
+    
+}
